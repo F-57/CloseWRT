34238c8bd1cf5e4a348bf5d2b61f6dfee4b95246
caiweill <80924478+caiweill@users.noreply.github.com>
2025-04-01 16:08:55 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -26,20 +26,20 @@ CFG_DTS="./target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-
 sed -i "s/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/g" $CFG_DTS
 
 # 更改菜单名字
-echo -e "\nmsgid \"MosDNS\"" >> package/mosdns/luci-app-mosdns/po/zh_Hans/mosdns.po
-echo -e "msgstr \"转发分流\"" >> package/mosdns/luci-app-mosdns/po/zh_Hans/mosdns.po
+echo -e "\nmsgid \"MosDNS\"" >> luci-app-mosdns/luci-app-mosdns/po/zh_Hans/mosdns.po
+echo -e "msgstr \"转发分流\"" >> luci-app-mosdns/luci-app-mosdns/po/zh_Hans/mosdns.po
 
-echo -e "\nmsgid \"Lucky\"" >> package/lucky/luci-app-lucky/po/zh_Hans/lucky.po
-echo -e "msgstr \"大吉大利\"" >> package/lucky/luci-app-lucky/po/zh_Hans/lucky.po
+echo -e "\nmsgid \"Lucky\"" >> luci-app-lucky/luci-app-lucky/po/zh_Hans/lucky.po
+echo -e "msgstr \"大吉大利\"" >> luci-app-lucky/luci-app-lucky/po/zh_Hans/lucky.po
 
 #echo -e "\nmsgid \"AList\"" >> package/alist/luci-app-alist/po/zh_Hans/alist.po
 #echo -e "msgstr \"聚合网盘\"" >> package/alist/luci-app-alist/po/zh_Hans/alist.po
 
-echo -e "\nmsgid \"Tailscale\"" >> package/luci-app-tailscale/po/zh_Hans/tailscale.po
-echo -e "msgstr \"虚拟组网\"" >> package/luci-app-tailscale/po/zh_Hans/tailscale.po
+echo -e "\nmsgid \"Tailscale\"" >> luci-app-tailscale/po/zh_Hans/tailscale.po
+echo -e "msgstr \"虚拟组网\"" >> luci-app-tailscale/po/zh_Hans/tailscale.po
 
-echo -e "\nmsgid \"Nikki\"" >> package/nikki/luci-app-nikki/po/zh_Hans/nikki.po
-echo -e "msgstr \"科学上网\"" >> package/nikki/luci-app-nikki/po/zh_Hans/nikki.po
+echo -e "\nmsgid \"Nikki\"" >> OpenWrt-nikki/luci-app-nikki/po/zh_Hans/nikki.po
+echo -e "msgstr \"科学上网\"" >> OpenWrt-nikki/luci-app-nikki/po/zh_Hans/nikki.po
 
 echo -e "\nmsgid \"UPnP\"" >> package/mtk/applications/luci-app-upnp-mtk-adjust/po/zh_Hans/upnp.po
 echo -e "msgstr \"即插即用\"" >> package/mtk/applications/luci-app-upnp-mtk-adjust/po/zh_Hans/upnp.po
